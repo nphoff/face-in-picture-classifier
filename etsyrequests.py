a8d7bb05eac09132dd4afa55ddce1f82f15e33a4
Victoria Mo <vm2355@columbia.edu>
2012-08-04 17:46:22 -0400

    added get transactions for shops

--- etsyrequests.py
+++ etsyrequests.py
@@ -34,21 +34,40 @@ def get_sellers_for_listings():
 				g.write(str(listing_id['user_id']) + '\n')
 
 def get_shops_for_sellers(limit):
+	numshops = 0
 	with open('../cached/active_sundress_sellers.txt','r') as f:
 		with open('../cached/active_sundress_shops.txt','w') as g:
 			for user_id in f:
+				print numshops
 				user_id = user_id.strip()
 				url = 'http://openapi.etsy.com/v2/users/' + str(user_id) + '/shops?limit=' + str(limit) + '&api_key=' + api_key
 				r = requests.get(url)
 				j = r.json['results']
 				for k in j:
+					numshops = numshops + 1
 					g.write(str(k['shop_id']) + '\n')
+
+def get_transactions_for_shops(limit):
+	numtrans = 0
+	with open('../cached/active_sundress_shops.txt','r') as f:
+		with open('../cached/active_sundress_transactions.txt','w') as g:
+			for shop_id in f:
+				print numtrans
+				shop_id = shop_id.strip()
+				url = 'http://openapi.etsy.com/v2/shops/' + str(shop_id) + '/transactions?limit=' + str(limit) + '&api_key=' + api_key
+				r = requests.get(url)
+				print r.content
+				j = r.json['results']
+				for k in j:
+					numtrans = numtrans + 1
+					g.write(str(k['transaction_id']) + '\n')
 
 def main():
 	limit = 100
 	#get_active_listings(limit) #comment out after cached
 	#get_sellers_for_listings()
-	get_shops_for_sellers(limit)
+	#get_shops_for_sellers(limit)
+	get_transactions_for_shops(limit)
 	print "done!"
 
 
