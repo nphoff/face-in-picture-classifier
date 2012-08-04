93c763e776920cec098d3a3b21ef33bb6b408012
nphoff <hoffman.nathan89@gmail.com>
2012-08-04 15:50:56 -0400

    commented out testing for image processing

--- imagegrabber.py
+++ imagegrabber.py
@@ -16,25 +16,26 @@ import cv
 #comment this line if you re-defined v2
 from keys import v2 as apiKey
 
-MAX_ITEMS = 5
+
 
 class ImageGrabber(object):
-    def __init__(self,sandbox=False):
+    def __init__(self,maxItems=5,sandbox=False):
         if sandbox:
             self.baseUrl = 'http://sandbox.openapi.etsy.com/v2'
         else:
             self.baseUrl = 'http://openapi.etsy.com/v2'
         self.urlPayload = {'api_key': apiKey}
-        self.listing_ids = []
+        self.listingIds = []
         self.targets = []
         self.target_ids = []
         self.hc = cv.Load("haarcascade_frontalface_default.xml")
-    
+        self.maxItems = maxItems
+
     def get_category_id(self, topCategory, subCategory=None,
                         subSubCategory = None):
         if subCategory:
             if subSubCategory:
-                r = requests.get(self.baseUrl + "/categories/" +  topCategory 
+                r = requests.get(self.baseUrl + "/categories/" +  topCategory
                                  + "/" + subCategory + "/" + subSubCategory,
                                  params=self.urlPayload)
             else:
@@ -47,12 +48,14 @@ class ImageGrabber(object):
 
     def get_category_listing_ids(self,category):
         payload = self.urlPayload
-        payload['limit'] = MAX_ITEMS
-        s = requests.get(self.baseUrl + "/listings/active/" + category,
+        payload['limit'] = self.maxItems
+        payload['category'] = category
+        s = requests.get(self.baseUrl + "/listings/active/",
                          params=payload)
-        
+        for entry in s.json['results']:
+            self.listing_ids.append(entry['listing_id'])
 
-    def get_listing_image(self, listing_id):
+def get_listing_image(self, listing_id):
         im = requests.get(self.baseUrl + "/listings/" + str(listing_id) + "/images",
                           params=self.urlPayload)
         results = im.json['results']
@@ -62,11 +65,14 @@ class ImageGrabber(object):
             break #take one.
         urllib.urlretrieve(imageUrl, 'temp')
         tempimage = cv.LoadImage('temp')
-        cv.NamedWindow('Boo!')
-        cv.ShowImage('Boo!',tempimage)
-        cv.WaitKey(0)
-        
-               
+
+        #cv.NamedWindow('Boo!')
+        #cv.ShowImage('Boo!',tempimage)
+        #cv.WaitKey(0)
+
 if __name__ == '__main__':
     a = ImageGrabber()
-    a.get_listing_image(101188906)
+    a.get_category_listing_ids('clothing/shirt/men')
+    for image in a.listing_ids:
+        a.get_listing_image(image)
+
